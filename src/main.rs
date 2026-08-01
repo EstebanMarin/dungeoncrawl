03993391e62490b8c7974a0d414e688005dd36ab
EstebanMarin <estebanmarin002@gmail.com>
2026-08-01 13:36:38 -0500

    workgin

--- src/main.rs
+++ src/main.rs
@@ -12,53 +12,25 @@ mod prelude {
 use prelude::*;
 
 struct State {
-    score: i32,
-    mode: GameMode,
+    map: Map,
 }
 
 impl State {
     fn new() -> Self {
-        State {
-            mode: GameMode::Menu, 
-	    score: 0,
-        }
+        State { map: Map::new() }
     }
-    
-    fn main_menu(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print_centered(5, "Dungeon");
-        ctx.print_centered(8, "(p) Play game");
-        ctx.print_centered(9, "(q) quit game");
-    }
-
-    fn dead(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print_centered(5, "You are dead");
-        ctx.print_centered(8, "(p) Play game");
-        ctx.print_centered(9, "(q) quit game");
-    }
-
-}
-
-enum GameMode {
-    Menu,
-    Playing,
-    End,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        match self.mode {
-            GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.main_menu(ctx),
-            GameMode::End => self.dead(ctx),
-        }
+        ctx.cls();
+        self.map.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Flappy Dragon")
+        .with_title("Dungeon Crawler")
         .build()?;
     main_loop(context, State::new())
 }
