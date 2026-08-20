2098271b456d355a4ea82d7c7491028b769d461b
EstebanMarin <estebanmarin002@gmail.com>
2026-08-20 16:55:43 -0500

    working

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,8 +1,6 @@
-use crate::{
-    prelude::*,
-    systems::{entity_render::entity_render, map_renders::*, player_input::*},
-};
+use crate::prelude::*;
 
+mod collisions;
 mod entity_render;
 mod map_renders;
 mod player_input;
@@ -10,6 +8,7 @@ mod player_input;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_renders::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
