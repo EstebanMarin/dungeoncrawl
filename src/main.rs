2126e7760c1c70f457d17d8e90ddb72193fd4d43
EstebanMarin <estebanmarin002@gmail.com>
2026-08-20 19:19:54 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod map;
 mod map_builder;
 mod spawner;
 mod systems;
+mod turn_states;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -21,6 +22,7 @@ mod prelude {
     pub use crate::map::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_states::*;
 }
 
 use prelude::*;
@@ -30,7 +32,9 @@ use crate::map_builder::MapBuilder;
 struct State {
     ecs: World,
     resources: Resources,
-    system: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
 }
 
 impl State {
@@ -48,10 +52,13 @@ impl State {
             .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
         Self {
             ecs,
             resources,
-            system: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
         }
     }
 }
@@ -63,7 +70,18 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.system.execute(&mut self.ecs, &mut self.resources);
+        let current_state = *self.resources.get::<TurnState>().unwrap();
+        match current_state {
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::PlayerTurn => self
+                .player_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
+        }
         render_draw_buffer(ctx).expect("Render error");
         // TODO: Render Draw Buffer
     }
