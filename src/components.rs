281ab91849cdc7f87c965a1f73515e19737cb4d4
EstebanMarin <estebanmarin002@gmail.com>
2026-08-20 20:30:03 -0500

    compiling

--- src/components.rs
+++ src/components.rs
@@ -18,5 +18,5 @@ pub struct MovingRandomly;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
     pub entity: Entity,
-    pub destination:  Point,
+    pub destination: Point,
 }
